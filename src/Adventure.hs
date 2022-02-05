c13692bd23fd787cdfb86e6b220bef96af68900e
Johan Ng <44483400+Johan-Ng@users.noreply.github.com>
2022-02-06 03:03:25 +0800

    Haskeline suggestions

--- src/Adventure.hs
+++ src/Adventure.hs
@@ -5,7 +5,10 @@ import Actions
 import Parsing
 
 import Control.Monad
+import Control.Monad.Trans
 import System.IO
+import System.Console.Haskeline
+import Data.List
 import System.Exit
 
 winmessage = "Congratulations, you have made it out of the house.\n" ++
@@ -19,27 +22,54 @@ process state input = case runParser input of
                             Just cmd -> actions cmd state
                             Nothing -> (state, "I don't understand")
 
-repl :: GameData -> IO GameData
+repl :: GameData -> InputT IO GameData
 repl state | finished state = return state
-repl state = do print state
-                putStr "What now? "
-                hFlush stdout
-                cmd <- getLine
-                if cmd == "save" then do save state
+repl state = do lift $ print state
+                lift $ putStr "What now? "
+                lift $ hFlush stdout
+                cmd <- getInputLine "> "
+                case cmd of
+                   Nothing -> return state
+                   Just "save" -> do lift $ save state
+                                     outputStrLn "Saved"
+                                     repl state
+                   Just cmd -> do let (state', msg) = process state cmd
+                                  outputStrLn msg
+                                  if (won state') then do outputStrLn winmessage
+                                                          return state'
+                                                else repl state'
+                {-if cmd == "save" then do save state
                                          putStrLn "Saved"
                                          repl state
                 else do
                 let (state', msg) = process state cmd
-                putStrLn msg
-                if (won state') then do putStrLn winmessage
+                outputStrLn msg
+                if (won state') then do outputStrLn winmessage
                                         return state'
-                               else repl state'
+                               else repl state' -}
 
-save :: GameData -> IO GameData
+--save :: GameData -> IO GameData
 save gd = do writeFile "save_data.txt" (show (location_id gd) ++ "\n" ++ show (inventory gd) ++ "\n" ++ show (poured gd) ++ "\n" ++ show (caffeinated gd))
              return gd
 
+-- Valid words for tab completion
+validWords :: [String]
+validWords = ["go","get", "put", "examine", "pour", "drink", "open", "wear", "use", "press", "inventory", "quit"]
+
+-- Given the string so far, return a list of possible combinations from
+-- 'validWords'
+completeValid :: String -> [Completion]
+completeValid inp = map simpleCompletion (filter (isPrefixOf inp) validWords)
+
+--Use the entire input so far, passing it in to 'completeValid'
+myCompletions :: CompletionFunc IO
+myCompletions (left, right) = return ([], --the input we didn't use
+                                   completeValid (reverse left))
+
+tes :: InputT IO ()
+tes = do repl initState
+         return ()
+
 
 main :: IO ()
-main = do repl initState
-          return ()
+main = runInputT (setComplete myCompletions defaultSettings) tes
