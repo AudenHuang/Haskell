aaa431e2a0c81dc2be836a474de5374c07b874a9
AudenHuang <35862858+AudenHuang@users.noreply.github.com>
2022-02-07 20:57:41 +0000

    Update Adventure.hs

--- src/Adventure.hs
+++ src/Adventure.hs
@@ -1,18 +1,19 @@
 module Main where
 
 import World
-import Actions
-import Parsing
+import Actions ( actions )
+import Parsing ( runParser )
 
 import Control.Monad
-import Control.Monad.Trans
-import System.IO
-import System.Console.Haskeline
-import Data.List
+import System.IO ( hFlush, stdout )
 import System.Exit
+import System.Directory (doesFileExist)
+import Distribution.Simple.Utils (lowercase)
+import Control.DeepSeq (rnf)
 
 winmessage = "Congratulations, you have made it out of the house.\n" ++
              "Now go to your lectures..."
+lossmessage = "Oh no! you die."
 
 {- Given a game state, and user input (as a list of words) return a 
    new game state and a message for the user. -}
@@ -22,54 +23,69 @@ process state input = case runParser input of
                             Just cmd -> actions cmd state
                             Nothing -> (state, "I don't understand")
 
-repl :: GameData -> InputT IO GameData
+repl :: GameData -> IO GameData
 repl state | finished state = return state
-repl state = do lift $ print state
-                lift $ putStr "What now? "
-                lift $ hFlush stdout
-                cmd <- getInputLine "> "
-                case cmd of
-                   Nothing -> return state
-                   Just "save" -> do lift $ save state
-                                     outputStrLn "Saved"
-                                     repl state
-                   Just cmd -> do let (state', msg) = process state cmd
-                                  outputStrLn msg
-                                  if (won state') then do outputStrLn winmessage
-                                                          return state'
-                                                else repl state'
-                {-if cmd == "save" then do save state
+repl state = do startgame state
+                putStr "\nWhat now?\n\n"
+                hFlush stdout
+                cmd <- getLine
+                if cmd == "save" then do save state
                                          putStrLn "Saved"
                                          repl state
+                else if cmd == "load" then do putStrLn "Loaded Save Data"
+                                              load
                 else do
                 let (state', msg) = process state cmd
-                outputStrLn msg
-                if (won state') then do outputStrLn winmessage
-                                        return state'
-                               else repl state' -}
+                putStrLn msg
+                if won state' 
+                then do putStrLn winmessage
+                        return state'
+                else if poisoned state'
+                then do putStr lossmessage
+                        return state'
+                else repl state'
 
---save :: GameData -> IO GameData
-save gd = do writeFile "save_data.txt" (show (location_id gd) ++ "\n" ++ show (inventory gd) ++ "\n" ++ show (poured gd) ++ "\n" ++ show (caffeinated gd))
+
+
+startgame :: GameData -> IO ()
+startgame state = if lighton state 
+                  then do print state
+                  else do putStrLn "The room is dark you can't see anything.\nYou can feel a light switch next to your hand.\nPleas press the switch to turn the lights on."
+
+
+save :: GameData -> IO GameData
+save gd = do writeFile "save_data.txt" (show (location_id gd) ++ "\n" ++ show (inventory gd) ++ "\n" 
+               ++ show (caffeinated gd) ++ "\n" ++ show (lighton gd)++ "\n" ++ show (maskon gd)++ "\n" ++
+                show (objects (getIndivRoom gd Bedroom)) ++ "\n" ++ show (objects (getIndivRoom gd Kitchen)) ++ "\n" ++ 
+                show (objects (getIndivRoom gd Hall)) ++ "\n" ++ show (objects (getIndivRoom gd LivingRoom))++ "\n" ++ 
+                show (objects (getIndivRoom gd DinningRoom)))
              return gd
 
--- Valid words for tab completion
-validWords :: [String]
-validWords = ["go","get", "put", "examine", "pour", "drink", "open", "wear", "use", "press", "inventory", "quit"]
+load = do inFile <- openFile "save_data.txt" ReadMode
+          gd <- hGetContents inFile
+          rnf gd `seq` hClose inFile
+          let saveData = lines gd
+              defState = GameData Bedroom gameworld [] False False False
+              currentRoom = parseRoom (saveData!!0)
+              currentInv = parseInv (splitInv (saveData!!1))
+              caffState = parseBool (saveData!!2)
+              lightState = parseBool (saveData!!3)
+              maskState = parseBool (saveData!!4)
+              wrld = [(Bedroom, worldState defState Bedroom (splitInv(saveData!!5))),
+                      (Kitchen, worldState defState Kitchen (splitInv(saveData!!6))),
+                      (Hall, worldState defState Hall (splitInv(saveData!!7))),
+                      (LivingRoom, worldState defState LivingRoom (splitInv(saveData!!8))),
+                      (DinningRoom, worldState defState DinningRoom (splitInv(saveData!!9)))
+                      (Street, street)]
+              initState = GameData currentRoom wrld currentInv caffState lightState maskState False False
+          do repl initState
 
--- Given the string so far, return a list of possible combinations from
--- 'validWords'
-completeValid :: String -> [Completion]
-completeValid inp = map simpleCompletion (filter (isPrefixOf inp) validWords)
-
---Use the entire input so far, passing it in to 'completeValid'
-myCompletions :: CompletionFunc IO
-myCompletions (left, right) = return ([], --the input we didn't use
-                                   completeValid (reverse left))
-
-tes :: InputT IO ()
-tes = do repl initState
-         return ()
+worldState :: GameData -> RoomID -> [[Char]] -> Room
+worldState gd rmid items = Room (room_desc (getIndivRoom gd rmid))
+                                (exits (getIndivRoom gd rmid))
+                                (parseInv items)
 
 
 main :: IO ()
-main = runInputT (setComplete myCompletions defaultSettings) tes
+main = do repl initState
+          return ()
